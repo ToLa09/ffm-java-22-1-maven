b8644fa6e892313820da8a97c259e9cb59380332
ToLa09 <landgraf-tobias@web.de>
2022-09-28 17:02:36 +0200

    Add faculty calculation with tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,10 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println(sum(2,5));
         System.out.println(checkNumberGreaterThan100(105));
+
+        System.out.println(faculty(3));
+        System.out.println(faculty(4));
+        System.out.println(faculty(5));
     }
 
     public static int sum(int value1, int value2) {
@@ -12,5 +16,18 @@ public class Main {
     public static boolean checkNumberGreaterThan100(int number) {
 
         return number>100;
+    }
+
+    public static int faculty(int number) {
+        int result = 1;
+        int i = 1;
+        while (i <= number){
+            result *= i;
+            i++;
+        }
+        //for(int i = 1; i <= number; i++) {
+        //    result *= i;
+        //}
+        return result;
     }
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -39,4 +39,64 @@ public class MainTest {
         //then
         assertFalse(actual);
     }
+    @Test
+    public void faculty0Is1(){
+        // given
+        int number = 0;
+
+        // when
+        int actual = Main.faculty(number);
+
+        // then
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+   @Test
+    public void faculty1Is1() {
+        // given
+        int number = 1;
+
+        // when
+        int actual = Main.faculty(number);
+
+        // then
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void faculty2Is2() {
+        // given
+        int number = 2;
+
+        // when
+        int actual = Main.faculty(number);
+
+        // then
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void faculty3Is6() {
+        // given
+        int number = 3;
+
+        // when
+        int actual = Main.faculty(number);
+
+        // then
+        int expected = 6;
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void faculty10IsBig() {
+        // given
+        int number = 10;
+
+        // when
+        int actual = Main.faculty(number);
+
+        // then
+        int expected = 3628800;
+        assertEquals(expected, actual);
+    }
 }
